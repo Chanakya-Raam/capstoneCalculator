f29ce60a7e7fd5f3b5b4aa9ad8f02575490d22b4
abhigodala <godala.abhinavareddy20@st.niituniversity.in>
2023-02-28 17:05:30 +0530

    taking user input

--- calculator.py
+++ calculator.py
@@ -7,4 +7,23 @@ def multiply(num1, num2):
     return num1 * num2
 
 def divide(num1, num2):
+    return num1 / num2# define a function for each arithmetic operation
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
     return num1 / num2
+
+# take input from user
+print("Please select operation -\n" \
+        "1. Add\n" \
+        "2. Subtract\n" \
+        "3. Multiply\n" \
+        "4. Divide\n")
+
