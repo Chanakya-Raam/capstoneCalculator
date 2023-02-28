cdcf35a8b79e9141e06f193c1dbb12dd98609699
Chanakya-Raam <chanakya.terala20@st.niituniversity.in>
2023-02-28 17:38:26 +0530

    created operating functions and printing result

--- calculator.py
+++ calculator.py
@@ -1,13 +1,4 @@
-def add(num1, num2):
-    return num1 + num2
-
-def subtract(num1, num2):
-    return num1 - num2
-def multiply(num1, num2):
-    return num1 * num2
-
-def divide(num1, num2):
-    return num1 / num2# define a function for each arithmetic operation
+# define a function for each arithmetic operation
 def add(num1, num2):
     return num1 + num2
 
@@ -27,3 +18,23 @@ print("Please select operation -\n" \
         "3. Multiply\n" \
         "4. Divide\n")
 
+choice = int(input("Enter choice (1/2/3/4): "))
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# perform the selected operation
+if choice == 1:
+    print(num1, "+", num2, "=", add(num1, num2))
+
+elif choice == 2:
+    print(num1, "-", num2, "=", subtract(num1, num2))
+
+elif choice == 3:
+    print(num1, "*", num2, "=", multiply(num1, num2))
+
+elif choice == 4:
+    print(num1, "/", num2, "=", divide(num1, num2))
+
+else:
+    print("Invalid input")
